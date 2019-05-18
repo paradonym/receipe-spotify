f7c94c4be0ab8b436d45a173cc4c9dcdf4979090
Konstantin <paradonym@users.noreply.github.com>
2019-05-18 19:01:33 +0200

    Update webview.js
    copied recipe-skype/webview.js in hope of that's also working for spotify, because of similar functions in playing audio and so on.

--- webview.js
+++ webview.js
@@ -1,2 +1,22 @@
-module.exports = () => {};
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    let count = 0;
+    const container = document.querySelector('[role="tablist"] > [title="Chats"] > div');
+    if (container) {
+      const children = container.children;
 
+      if (children.length === 3) {
+        const elementContainer = children[children.length - 1];
+        if (elementContainer) {
+          const element = elementContainer.querySelector('[data-text-as-pseudo-element]');
+          count = parseInt(element.dataset.textAsPseudoElement, 10);
+        }
+      }
+    }
+
+    // set Franz badge
+    Franz.setBadge(count);
+  };
+
+  Franz.loop(getMessages);
+};
