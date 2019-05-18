ef3c97f7e0da83dd85877506137c9e66cac341fb
Konstantin <paradonym@users.noreply.github.com>
2019-05-18 19:02:48 +0200

    Update index.js
    copied recipe-skype/index.js in hope of that's also working for Spotify Web

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-module.exports = Franz => class CustomWebsite extends Franz {
-  // events = {
-  //   'page-favicon-updated': '_setFavicon',
-  // }
+"use strict";
 
-  async validateUrl(url) {
-    try {
-      const resp = await window.fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return resp.status.toString().startsWith('2') || resp.status.toString().startsWith('3');
-    } catch (err) {
-      console.error(err);
-    }
-
-    return false;
+module.exports = Franz => class Messenger extends Franz {
+  overrideUserAgent() {
+    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '');
   }
 
-  // _setFavicon(e) {
-  // TODOS: need better APIs for that
-  // }
 };
